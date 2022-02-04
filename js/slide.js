0e4f78a8c0086c087c6d2d36ea84b1e87c89bfa4
Alex <alexgouveiadearaujo@gmail.com>
2022-02-04 15:33:49 -0300

    remove console.log

--- js/slide.js
+++ js/slide.js
@@ -33,7 +33,8 @@ export default class Slide {
   }
 
   onMove(e) {
-    const pointerPosition = e.type === "mousemove" ? e.clientX : e.changedTouches[0].clientX;
+    const pointerPosition =
+      e.type === "mousemove" ? e.clientX : e.changedTouches[0].clientX;
     const finalPosition = this.updatePosition(pointerPosition);
     this.moveSlide(finalPosition);
   }
@@ -57,9 +58,43 @@ export default class Slide {
     this.onEnd = this.onEnd.bind(this);
   }
 
+  slidePosition(slide) {
+    const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+    return -(slide.offsetLeft - margin);
+  }
+
+  slidesConfig() {
+    this.slideArray = [...this.slide.children].map((element) => {
+      const position = this.slidePosition(element);
+      return {
+        position,
+        element,
+      };
+    });
+    console.log(this.slideArray);
+  }
+
+  slideIndexNav(index) {
+    const last = this.slideArray.length - 1
+    this.index = {
+      prev:  index ? index - 1 : undefined,
+      active: 0,
+      next: index === last ? undefined : index + 1,
+    };
+  }
+
+  changeSlide(index) {
+    const activeSlide = this.slideArray[index]
+    this.moveSlide(activeSlide.position);
+    this.slideIndexNav(index)
+    this.dist.finalPosition = activeSlide.position
+  }
+
   init() {
     this.bindEvent();
     this.addSlideEvents();
+    this.slidesConfig();
+    this.changeSlide(0)
     return this;
   }
 }
